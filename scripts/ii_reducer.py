1fb2b79fd01c97373ed37c51087636d7d9500aa8
juan1t0 <juanpablo.heredia@ucsp.edu.pe>
2020-10-14 11:52:18 -0500

    clean

--- scripts/ii_reducer.py
+++ scripts/ii_reducer.py
@@ -3,14 +3,12 @@
 from itertools import groupby
 from operator import itemgetter
 import sys
-import re
 
 def read_mapper_output(file, separator='\t'):
 	for line in file:
 		yield line.rstrip().split(separator, 1)
 
 def main(separator='\t'):
-	# index = {}
 	data = read_mapper_output(sys.stdin, separator=separator)
 	for current_word, group in groupby(data, itemgetter(0)):
 		try:
