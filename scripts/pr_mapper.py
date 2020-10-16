9a23be0dbcbc1216925726f414cac10461aa0a4b
juan1t0 <juanpablo.heredia@ucsp.edu.pe>
2020-10-16 13:22:59 -0500

    random edges

--- scripts/pr_mapper.py
+++ scripts/pr_mapper.py
@@ -6,40 +6,39 @@ import json
 from random import randint
 
 def fill_graph(graph):
-	n = len(graph)
-	for u in graph:
-		nv = graph[u][1]
-		id = graph[u][0]
-		vs = []
-		for i in range(nv):
-			v = randint(0,n-1)
-			while v != u:
-				v = randint(0,n-1)
-			vs.append(v)
-		graph[u] = (id, vs)
-	return graph
-	
+        n = len(graph)
+        for u in graph:
+                nv = graph[u][1]
+                id = graph[u][0]
+                vs = []
+                for i in range(nv):
+                        v = randint(0,n-1)
+                        while v == u:
+                                v = randint(0,n-1)
+                        vs.append(v)
+                graph[u] = (id, vs)
+        return graph
+
 def read_input(file, graph, separator='\t'):
-	i = 0
-	for line in file:
+        for line in file:
                 paper = json.loads(line)
                 if not paper.has_key('abstract'):
-			continue
-                refs = randint(2,16)#len(paper['references'])
+                        continue
+                refs = randint(3,12)#len(paper['references'])
                 yield (paper['id'], refs)
-		graph[i] = (paper['id'], refs)
-		i+=1
-	graph = fill_graph(graph)
-		
-def main(separator='\t'):
-	graph = {}
-	data = read_input(stdin, graph, separator)
-	for paper_id, n_ref in data:
-		val = 1.0 / n_ref
-		print '%s%s%f' % (paper_id, separator, val)
-	for u in graph:
-		for v in graph[u][1]:
-			print '%s%s%s' % (graph[u][0], separator, graph[v][0])
+                graph[i] = (paper['id'], refs)
+        graph = fill_graph(graph)
 
+def main(separator='\t'):
+        graph = {}
+        data = read_input(stdin, graph, separator)
+        for paper_id, n_ref in data:
+                val = 1.0 / n_ref
+                print '%s%s%f' % (paper_id, separator, val)
+	for u in graph:
+                for v in graph[u][1]:
+                        print '%s%s%s' % (graph[u][0], separator, graph[v][0])
+        #print graph
 if __name__ == "__main__":
-	main()
+        main()
+
