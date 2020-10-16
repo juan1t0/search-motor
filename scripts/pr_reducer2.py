202067a63c5e9055558db5ebf498c22d5a4f0a99
juan1t0 <juanpablo.heredia@ucsp.edu.pe>
2020-10-16 13:25:22 -0500

    pagerank

--- scripts/pr_reducer2.py
+++ scripts/pr_reducer2.py
@@ -4,36 +4,35 @@
 import sys
 
 def read_mapper_output(file, separator='\t'):
-	graph, weightsn, weightsc = {}, {}, {}
-	for line in file:
-		dt = line.rstrip().split(separator, 1)
-		try:
-			dt[1] = float(dt[1])
-			weightsn[dt[0]]= 0.0
-			weightsc[dt[0]]= dt[1]
-		except:
-                        if graph.has_key(dt[0])
+        graph, weightsn, weightsc = {}, {}, {}
+        for line in file:
+                dt = line.rstrip().split(separator, 1)
+                try:
+                        dt[1] = float(dt[1])
+                        weightsn[dt[0]]= 0.0
+                        weightsc[dt[0]]= dt[1]
+                except:
+                        if graph.has_key(dt[0]):
                                 graph[dt[0]].append(dt[1])
                         else:
-                                graph[dt[0]] = [dt[1]
-	return graph, weightsn, weightsc
+                                graph[dt[0]] = [dt[1]]
+        return graph, weightsn, weightsc
 
 def main(separator='\t'):
-	graph, currvals, newvals = read_mapper_output(sys.stdin, separator)
-	for nid in graph:
-		es = graph[nid]
-		for e in es:
-			print '%s%s%s' % (nid, separator, e)
-			try:
-				newvals[e] += currvals[nid]
-			except:
-				pass
-	for paper_id in newvals:
-		try:
-			w = newvals[paper_id] / float(len(graph[paper_id]))
-		except:
-			w = newvals[paper_id]
-		print '%s%s%f' % (paper_id, separator, w)
+        graph, currvals, newvals = read_mapper_output(sys.stdin, separator)
+        for nid in graph:
+                es = graph[nid]
+                for e in es:
+                        try:
+                                newvals[e] += currvals[nid]
+                        except:
+                                pass
+        for paper_id in newvals:
+                try:
+                        w = newvals[paper_id] / float(len(graph[paper_id]))
+                except:
+                        w = newvals[paper_id]
+                print '%s%s%f' % (paper_id, separator, w)
 
 if __name__ == "__main__":
-	main()
+        main()
